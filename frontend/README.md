f655e9364f3c54491348b1df012ac707848b59e8
Arif Shahriar <arifshahriar028@gmail.com>
2023-02-16 22:08:18 +0600

    added backend

--- frontend/README.md
+++ frontend/README.md
@@ -1,3 +1,40 @@
 # Criminal Record Management System
 
-Permissioned Block-Chain
+### How to run backend
+
+Open terminal and change directory to 'backend/test-network' and Then:
+
+**_ Start test network: _**
+
+```
+./network.sh createChannel -ca -c criminalrecord -s couchdb
+```
+
+**_ Install Chaincode: _**
+
+```
+./network.sh deployCC -ccn cRecord -ccp ../crab/chaincode/ -ccl javascript
+```
+
+**_ Stopping the network: _**
+
+```
+./network.sh down
+```
+
+**_ CouchDB: _**
+`http://localhost:5984/_utils/`
+username: admin
+pass: adminpw
+
+**_ Run express server: _**
+Change directory to 'backend/crab/application' and then:
+
+```
+npm install
+node app.js
+```
+
+**_ Test api _**
+Install "REST Client" extention on vscode (extension id: humao.rest-client).
+Then use `backend/crab/application/api-test.rest` file to test the api.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team/index";
-import Contacts from "./scenes/contacts/index";
-import Form from "./scenes/form/index";
-import Home from "./scenes/home/index.jsx";
-import Login from "./scenes/login/index.jsx";
+import React from 'react';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './scenes/dashboard';
+import Team from './scenes/team/index';
+import Contacts from './scenes/contacts/index';
+import Form from './scenes/form/index';
+import Home from './scenes/home/index.jsx';
+import Login from './scenes/login/index.jsx';
 
 function App() {
-  
   return (
-    
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard/>} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/team" element={<Team />} />
-      <Route path="/contacts" element = {<Contacts />} />
+      <Route path="/contacts" element={<Contacts />} />
       <Route path="/form" element={<Form />} />
     </Routes>
   );
